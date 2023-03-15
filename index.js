83c9c94db23b7d26372441b9f8603cf2374a296e
Michal <khakaimic4@gmail.com>
2023-03-15 22:04:17 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
+if (distance <=400) {
   // Write your code here!
+return "This one is on me!";
+}
+else if (distance >2000 && distance <=2500){
+return "I will gladly take your thirty bucks.";
+}
+else if (distance >2500){
+  return "No can do.";
+  
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good.": "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip (Tip) {
+  switch (Tip) {
+    case"generous": return "Thank you so much.";
+    case "not as generous": return "Thank you.";
+    default: return "Bye.";
+  }
+
 }
+
+
